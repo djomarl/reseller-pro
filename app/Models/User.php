b2605379b7a3edefe00c5364b95696e53c6d14bf
Djomar <djomar@ladiesnight.nl>
2026-02-05 16:14:29 +0100

    Fixed user met presets

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+use App\Models\ItemTemplate;
 
 class User extends Authenticatable
 {
@@ -44,5 +47,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function itemTemplates(): HasMany
+    {
+        return $this->hasMany(ItemTemplate::class);
     }
 }
